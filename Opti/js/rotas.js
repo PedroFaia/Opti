1b129c6e1e32289d985cbd24d7bf881edcc74fde
PedroFaia <pedro.faia@hotmail.com>
2018-09-18 01:10:16 -0300

    https://github.com/PedroFaia/Opti.git

---
+++ Opti/js/rotas.js
@@ -0,0 +1,15 @@
+﻿function rediLogin() {
+
+    var urlEntrar = window.location.pathname.replace("UsuarioCad", "") + "Login";
+
+    window.location.href = (urlEntrar);
+
+}
+
+function rediCad() {
+
+    var urlEntrar = window.location.pathname.replace("Login", "") + "UsuarioCad";
+
+    window.location.href = (urlEntrar);
+
+}
